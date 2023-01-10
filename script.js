7701a083c689db7aef83fcbffc9685684139f836
Charonmortis <101378229+Charonmortis@users.noreply.github.com>
2023-01-10 21:51:34 +0100

    Created scripts for making the input field functional

--- script.js
+++ script.js
@@ -5,29 +5,62 @@
 const INPUT_FIELD = document.querySelector('#currentResult')
 
 INPUT_FIELD.addEventListener('keydown', (e)=>{
-    if (
-        e.key == '0' ||
-        e.key == '1' ||
-        e.key == '2' ||
-        e.key == '3' ||
-        e.key == '4' ||
-        e.key == '5' ||
-        e.key == '6' ||
-        e.key == '7' ||
-        e.key == '8' ||
-        e.key == '9' ||
-        
-        e.key == '/' ||
-        e.key == '*' ||
-        e.key == '-' ||
-        e.key == '+' ||
-
-        e.key == 'Backspace' ||
-        e.key == 'Delete'
-        ) {
-            return
-    } else {
-        e.preventDefault()
-    }
+    e.preventDefault()
 })
 
+/**
+ * MAKING UI BUTTONS FUNCTIONALS
+ */
+
+let operatorPressed = false
+let byMinusOne = false
+let operationLength = 0
+
+const NUMBER_BTN = Array.from(document.querySelectorAll('.numberBtn'))
+const CLEAR_BTN = document.querySelector('#clear')    
+const BY_MINUS_ONE = document.querySelector('#byMinusOne')
+
+NUMBER_BTN.forEach(btn => 
+    btn.addEventListener('click', (e)=>{
+        if (operationLength < 24) {
+            INPUT_FIELD.value += `${e.target.dataset.value}`
+            operatorPressed = false
+            operationLength++
+        }
+    })
+    )
+    
+    const OPERATOR_BTN = Array.from(document.querySelectorAll('.aOperators'))
+    
+    OPERATOR_BTN.forEach(btn => 
+        btn.addEventListener('click', (e)=>{
+            if (operationLength < 24) {
+                if (!operatorPressed) {
+                    INPUT_FIELD.value += `${e.target.dataset.value}`
+                    operatorPressed = true
+                    operationLength++
+                } else{
+                    INPUT_FIELD.value = INPUT_FIELD.value.substring(0, INPUT_FIELD.value.length - 1);
+                    INPUT_FIELD.value += `${e.target.dataset.value}`
+                }
+                setCaretPosition(INPUT_FIELD, INPUT_FIELD.value.length);
+            }
+        })
+        
+        )
+        
+        const ALL_CLEAR_BTN = document.querySelector('#ac')    
+        
+ALL_CLEAR_BTN.addEventListener('click', ()=>{
+    INPUT_FIELD.value = ''
+    operationLength = 0
+})
+
+CLEAR_BTN.addEventListener('click', ()=>{
+    INPUT_FIELD.value = INPUT_FIELD.value.substring(0, INPUT_FIELD.value.length - 1);
+    operationLength--
+})
+
+BY_MINUS_ONE.addEventListener('click', ()=>{
+    INPUT_FIELD.value = `-${INPUT_FIELD.value}`
+})
